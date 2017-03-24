0b55726a06dfd26c66fcf6873cc27434a0279a65
NVS Abhilash <nvs232@gmail.com>
2017-03-24 14:42:41 +0530

    Added login module

--- commonMan/src/app/app.module.ts
+++ commonMan/src/app/app.module.ts
@@ -6,9 +6,24 @@ import { Page2 } from '../pages/page2/page2';
 import { LoginPage } from '../pages/login/login';
 import { RegisterPage } from '../pages/register/register';
 import { IonicService } from '../providers/ionic-service';
-
+import { AuthConfig, AuthHttp } from 'angular2-jwt';
+import { AuthService } from '../providers/auth-service';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { ProfilePage } from '../pages/profile/profile';
+
+import { Http } from '@angular/http';
+import { Storage } from '@ionic/storage';
+
+let storage: Storage = new Storage();
+
+export function getAuthHttp(http) {
+  return new AuthHttp(new AuthConfig({
+    globalHeaders: [{'Accept': 'application/json'}],
+    tokenGetter: (() => storage.get('id_token'))
+  }), http);
+}
 
 @NgModule({
   declarations: [
@@ -33,7 +48,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     StatusBar,
     SplashScreen,
     IonicService,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService,
+    {
+      provide: AuthHttp,
+      useFactory: getAuthHttp,
+      deps: [Http]
+    }
+    
   ]
 })
 export class AppModule {}
