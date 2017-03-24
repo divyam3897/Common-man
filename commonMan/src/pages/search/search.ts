70828b9d1422d8ceb0b8dffdcc95c9079218bea8
ashivliving <ashivgupta23@gmail.com>
2017-03-24 21:41:41 +0530

    search functionality complete

--- commonMan/src/pages/search/search.ts
+++ commonMan/src/pages/search/search.ts
@@ -35,7 +35,22 @@ export class SearchPage {
     ); 
   }
 
-  
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.ItemName.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
 
 
 }
